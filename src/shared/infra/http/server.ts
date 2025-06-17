780595247962a3f2c1f9979291306059d4d730bf
HugoZAuad <hugozeymer@hotmail.com>
2025-06-17 20:53:15 -0300

    fix error cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -41,21 +41,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-  preflightContinue: true,
-  optionsSuccessStatus: 204,
-}));
-
-app.options('*', cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
   optionsSuccessStatus: 204,
 }));
 app.use(express.json());
