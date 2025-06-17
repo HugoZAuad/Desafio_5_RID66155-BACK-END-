9e33a4e2a6af4d36738dbb8be0680dbdaa731714
HugoZAuad <hugozeymer@hotmail.com>
2025-06-17 20:45:34 -0300

    Fix books entities

--- src/modules/books/infra/database/entities/Book.ts
+++ src/modules/books/infra/database/entities/Book.ts
@@ -1,6 +1,6 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
 
-@Entity('livros')
+@Entity('books')
 export class livros  {
   @PrimaryGeneratedColumn()
   id: number;
