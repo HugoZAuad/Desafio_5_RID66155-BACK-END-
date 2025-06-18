96091406d4ab6334165bcbfd2275b73eb9aa1e15
HugoZAuad <hugozeymer@hotmail.com>
2025-06-17 21:05:44 -0300

    fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+const SUPABASE_JWT = process.env.SUPABASE_JWT || '2Fyf8BMfs7kgM7iBySQ5Lr4TbzHsmg/fXkCAOQKjrhdaifZOgCaEgahj45i84wGkJWLM3NHSNUWPfdtVlV7JLw==';
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
@@ -24,4 +26,7 @@ export const AppDataSource = new DataSource({
   ssl: {
     rejectUnauthorized: false,
   },
+  extra: {
+    options: `-c jwt.token=${SUPABASE_JWT}`
+  }
 });
