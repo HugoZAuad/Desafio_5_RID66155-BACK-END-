7331f34741b2c63167fc09e0089326df6b9b41f2
HugoZAuad <hugozeymer@hotmail.com>
2025-06-18 15:43:05 -0300

    new entitites

--- src/modules/books/infra/database/entities/Book.ts
+++ src/modules/books/infra/database/entities/Book.ts
@@ -1,25 +1,25 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
 
 @Entity('books')
-export class livros  {
+export class Book {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number
 
   @Column()
-  titulo: string;
+  titulo: string
 
   @Column()
-  pages: number;
+  pages: number
 
-    @Column()
-  ISBN: number;
-  
-    @Column()
-  editora: string;
+  @Column()
+  ISBN: number
+
+  @Column()
+  editora: string
 
   @CreateDateColumn()
-  created_at: Date;
+  created_at: Date
 
   @UpdateDateColumn()
-  updated_at: Date; 
+  updated_at: Date
 }
