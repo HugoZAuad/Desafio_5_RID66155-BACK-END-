59ceb23ebfe8dde8da78fcbf0990b2a5db3aef88
HugoZAuad <hugozeymer@hotmail.com>
2025-06-18 11:39:57 -0300

    fix

---
+++ src/shared/infra/http/index.ts
@@ -0,0 +1,3 @@
+import app from '@shared/infra/http/server';
+
+export default app;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,13 +4,12 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import 'express-async-errors';
 import { AppDataSource } from '../../../config/database';
-import BooksRoutes from '@modules/books/infra/http/routes/BooksRoutes';
+import BooksRoutes from '../../../modules/books/infra/http/routes/BooksRoutes';
 import ErrorHandleMiddleware from '@shared/middlewares/ErrorHandleMiddleware';
 
 dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT || 3000;
 
 AppDataSource.initialize()
   .then(() => {
@@ -34,11 +33,5 @@ app.use(errors());
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   ErrorHandleMiddleware.handleError(err, req, res, next);
 });
-
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-  });
-}
 
 export default app;
