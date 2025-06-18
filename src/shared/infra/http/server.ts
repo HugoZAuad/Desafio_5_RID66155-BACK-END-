ccd3168bc52f25ef13ba11113177f5e3ac1d1e71
HugoZAuad <hugozeymer@hotmail.com>
2025-06-17 23:17:41 -0300

    fix server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 import { errors } from 'celebrate';
 import dotenv from 'dotenv';
 import 'express-async-errors';
-import { AppDataSource } from '../../../config/database';
+import { AppDataSource } from '@config/database';
 import BooksRoutes from '@modules/books/infra/http/routes/BooksRoutes';
 import ErrorHandleMiddleware from '@shared/middlewares/ErrorHandleMiddleware';
 import { CorsMiddleware } from '@shared/middlewares/CorsMiddleware';
@@ -10,28 +10,22 @@ import { CorsMiddleware } from '@shared/middlewares/CorsMiddleware';
 dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT || 3000;
 
 app.use(CorsMiddleware);
 
 AppDataSource.initialize()
   .then(() => {
     console.log('Conexão com banco de dados estabelecida');
-    app.listen(port, () => {
-      console.log(`Servidor rodando na porta ${port}`);
-    });
   })
-  .catch((error) => {
+  .catch((error: any) => {
     console.error('Erro ao conectar com o banco de dados:', error);
-    console.error(error);
   });
 
 app.use(express.json());
-
 app.use('/livros', BooksRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('API de Livros funcionando!');
+  res.send('API de Livros funcionando na Vercel! 📚');
 });
 
 app.use(errors());
